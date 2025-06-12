fa1c26596855bf19ad09a6645fcdce3f13542428
ibbar <ibbar616@gmail.com>
2025-06-12 10:50:32 +0200

    fixed asset display for new evm native format

--- web3/api.ts
+++ web3/api.ts
@@ -1,19 +1,28 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
+import * as ethers from "ethers";
+
 declare let ethereum: any;
 
 export async function subProvider(network) {
   const chains = {
     moonbase: {
       ws: "wss://moonbase.unitedbloc.com",
+      https: "https://moonbase.unitedbloc.com",
+      chainId: 1287,
     },
     moonriver: {
       ws: "wss://moonriver.unitedbloc.com",
+      https: "https://moonriver.unitedbloc.com",
+      chainId: 1285,
     },
     moonbeam: {
       ws: "wss://moonbeam.unitedbloc.com",
+      https: "https://moonbeam.unitedbloc.com",
+      chainId: 1284,
     },
   };
 
+  // Instantiation of Polkadot API
   // Create WS Provider
   const wsProvider = new WsProvider(chains[network].ws);
 
@@ -23,5 +32,9 @@ export async function subProvider(network) {
     noInitWarn: true,
   });
   await api.isReady;
-  return api;
+
+  // Intsantiate Ethereum Provider
+  const ethAPI = new ethers.JsonRpcProvider(chains[network].https);
+
+  return [api, ethAPI];
 }
